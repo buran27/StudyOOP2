4c80145413b95b7d8560d237f5e6d360df3bfa1c
noko <limnotflybyl@gmail.com>
2026-04-28 18:23:23 +0300

    Calculator

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+using System;
+
+public static class PrimeCalculator
+{
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+    
+}

16808351c62d0ec7846d2506b12f314c47612eda
noko <limnotflybyl@gmail.com>
2026-04-28 18:49:44 +0300

    Print of Results in Calculator

--- Calculator.cs
+++ Calculator.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Diagnostics;
+public static class PrimeCalculator {
 
-public static class PrimeCalculator
-{
-    public static bool IsPrime(int number)
-    {
+    public const int ThreadCount = 4;
+    public static bool IsPrime(int number) {
+
         if (number < 2) return false;
-        for (int i = 2; i * i <= number; i++)
-        {
-            if (number % i == 0) return false;
+
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) return false;            
         }
+
         return true;
     }
-    
+
+    public static void ResultOfCalculations(string version, int count, TimeSpan timeOfProccessing) {
+        Console.WriteLine(version);
+        Console.WriteLine($"Найдено {count} простых чисенл");
+        Console.WriteLine($"Время работы {timeOfProccessing.TotalMilliseconds}");
+    }
+
 }

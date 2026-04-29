658eefe8e675e998b21a7bc3dcb06f6832a5f0c9
burka27 <bur.an.22303@bk.ru>
2026-04-29 15:23:47 +0300

    Create num set file generation

--- 1-2/NumSetFileProvider.cs
+++ 1-2/NumSetFileProvider.cs
@@ -1,4 +1,4 @@
-
+namespace Study.Generate;
 
 public class NumSetFileProvider
 {
@@ -18,9 +18,49 @@ public class NumSetFileProvider
   {
     if (string.IsNullOrWhiteSpace(path))
       throw new ArgumentException("Path to file can't be empty");
+    if (setsCount <= 0)
+      throw new ArgumentException("Sets count can't be <= 0");
 
+    if (numInSet <= 0)
+      throw new ArgumentException("Nums in set can't be <= 0");
+
+    if (max < min)
+      throw new ArgumentException("Max value can't be < min value");
 
       _filePath = path;
+      _setsCount = setsCount;
+      _numInSet = numInSet;
+      _minValue = min;
+      _maxValue = max;
+  }
+
+  public void EnsureFileExists()
+  {
+    if (File.Exists(_filePath))
+    {
+      Console.WriteLine($"Fail is created: {_filePath}");
+      Console.WriteLine("Generation data dont started");
+      return;
+    }
+    GenerateFile();
+  }
+
+  private void GenerateFile()
+  {
+    Random rand = new Random();
+
+    using StreamWriter writer = new StreamWriter(_filePath);
+    for(int i = 0; i < _setsCount; i++)
+    {
+      int[] nums = new int[_numInSet];
+      for(int j = 0; j < _numInSet; j++)
+      {
+        nums[j] = rand.Next(_minValue, _maxValue + 1);
+      }
+      string line = string.Join(" ", nums);
+      writer.WriteLine(line);
+    }
+    Console.WriteLine($"File is created: {_filePath}");
   }
 
 
